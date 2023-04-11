d1347d13c06122545b2bf51e333c331d3b76f8f0
GumSurf <gabriel.christe@gmail.com>
2023-04-11 20:40:54 +0200

    Add : file login.js et envois de e-mail et password à l'api

---
+++ Backend/js/login.js
@@ -0,0 +1,25 @@
+const form = document.querySelector(".form_login");
+const inputs = document.querySelectorAll("input");
+
+form.addEventListener("submit", (event) => {
+
+    // Désactivation du comportement par défaut du navigateur
+    event.preventDefault();
+
+    const email = inputs[0].value;
+    const password = inputs[1].value;
+
+    const emailPassword = {
+        email : email,
+        password : password
+    };
+    console.log("test : %s %s", email, password);
+
+    // Appel de la fonction fetch avec toutes les informations nécessaires
+    //**Commentaire à moi même ne pas mettre de s a "api"**
+    fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(emailPassword)
+    });
+});
