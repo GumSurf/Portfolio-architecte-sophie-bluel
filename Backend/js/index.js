781f3020e2effeb309a5051b0989a65aee2a9146
GumSurf <gabriel.christe@gmail.com>
2023-04-12 18:48:35 +0200

    Add : verification du token si existant et debut du mode d'édition de page

--- Backend/js/index.js
+++ Backend/js/index.js
@@ -1,1 +1,27 @@
 //je suis pas vide
+function editMode() {
+    const indexHeader = document.header;
+    const testH1 = document.h1;
+    const icon = document.getElementById('icon');
+    const modeEditionText = document.createElement('p');
+
+    modeEditionText.textContent = 'Mode Edition';
+
+    const body = document.querySelector("body");
+    body.before(modeEditionText);
+    //indexHeader.before(modeEditionText);
+    console.log("EDIT-MODE");
+}
+
+function main() {
+    //récupération du token
+    const loged = window.localStorage.getItem("tokenUser");
+    if(loged) {
+
+        editMode();
+        console.log("loged");
+        console.log("le token index = %s", loged);
+    }
+
+}
+main();
