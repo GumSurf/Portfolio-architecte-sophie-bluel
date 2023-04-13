efa25477630af6619f62c4824366bf73af9ecf37
GumSurf <gabriel.christe@gmail.com>
2023-04-13 12:23:10 +0200

    Add : modal gallery avec les photos et ajout modal add photo que en Html

--- Backend/js/index.js
+++ Backend/js/index.js
@@ -44,8 +44,6 @@ async function createModal() {
     const modal = document.querySelector(".modal");
     const divModal = await createBalise("div", "div_modal");
     const divButtonTopModal = await createBalise("button", "button_top_modal");
-    const iconButtonTop = await createBalise("i");
-    iconButtonTop.classList.add("fa-solid", "fa-xmark");
     const titreModal = await createBalise("h2", "titre_modal", "Gallerie photo");
     const moveIcon = await createBalise("i");
     moveIcon.classList.add("fa-solid", "fa-up-down-left-right");
@@ -56,12 +54,88 @@ async function createModal() {
 
     modal.appendChild(divModal);
     divModal.appendChild(divButtonTopModal);
-    divButtonTopModal.appendChild(iconButtonTop);
     divModal.appendChild(titreModal);
     divModal.appendChild(moveIcon);
     divModal.appendChild(deleteIcon);
     divModal.appendChild(buttonAddPhoto);
     divModal.appendChild(deleteAllGallery);
+
+    printWorksModal();
+}
+
+async function printWorksModal() {
+    myFetch = await fetch("http://localhost:5678/api/works");
+    myFetch = await myFetch.json();
+
+    const divModal = document.querySelector(".modal");
+    const iconButtonTop = await createBalise("i");
+    iconButtonTop.classList.add("fa-solid", "fa-xmark");
+
+    await divModal.appendChild(iconButtonTop);
+
+    for (let i = 0; i < myFetch.length; i++) {
+
+        const textEditer = await createBalise("p", "text_editer", "éditer");
+        const trashCanIcone = await createBalise("i");
+        trashCanIcone.classList.add("fa-solid", "fa-trash-can");
+        const worksModalImage = await createBalise("img");
+        worksModalImage.src = myFetch[i].imageUrl;
+
+        await divModal.appendChild(trashCanIcone);
+        await divModal.appendChild(worksModalImage);
+        await divModal.appendChild(textEditer);
+
+        console.log("\n*****SEPARATION*****\n");
+        console.log("myFetch = %s", myFetch[i]);
+        console.log("myFetch.id = %s", myFetch[i].id);
+        console.log("myFetch.title = %s", myFetch[i].title);
+        console.log("myFetch.imageUrl = %s", myFetch[i].imageUrl);
+        console.log("myFetch.categoryId = %s", myFetch[i].categoryId);
+        console.log("myFetch.userId = %s", myFetch[i].userId);
+        console.log("myFetch.category = %s", myFetch[i].category);
+        console.log("myFetch.category.id = %s", myFetch[i].category.id);
+        console.log("myFetch.category.name = %s", myFetch[i].category.name);
+        console.log("\n*****SEPARATION*****\n");
+    }
+}
+
+//function création de la modal pour ajouter des photos
+async function addPhotoGallery() {
+    const divModalAddPhoto = document.querySelector(".modal_add_photo");
+    const arrowLeftIcon = await createBalise("i");
+    arrowLeftIcon.classList.add("fa-solid", "fa-arrow-left");
+    const titleModalAddPhoto = await createBalise("h3", "title_modal_add_photo", "Ajout photo");
+    const iconButtonTop = await createBalise("i");
+    iconButtonTop.classList.add("fa-solid", "fa-xmark");
+    const sectionAddPhoto = await createBalise("section", "section_add_photo");
+    const iconPicture = await createBalise("i");
+    iconPicture.classList.add("fa-thin", "fa-image");
+    const buttonAddPhoto = await createBalise("button", "button_add_photo", "+ Ajouter photo");
+    const textMaxMo = await createBalise("p", "text_max_mo", "jpg.png : 4mo max");
+    const sectionForm = await createBalise("section", "section_form");
+    const form = await createBalise("form", "form");
+    const formLabelTitre = await createBalise("label", "form_label_titre", "Titre");
+    const formInputTitre = await createBalise("input", "form_input_titre");
+    const formLabelCategorie = await createBalise("label", "form_label_categorie", "Catégorie");
+    const formInputCategorie = await createBalise("input", "form_input_categorie");
+    const divLiseretGris = await createBalise("div", "div_liseret_gris");
+    const formButtonValider = await createBalise("button", "form_button_valider", "Valider");
+
+    await divModalAddPhoto.appendChild(arrowLeftIcon);
+    await divModalAddPhoto.appendChild(titleModalAddPhoto);
+    await divModalAddPhoto.appendChild(iconButtonTop);
+    await divModalAddPhoto.appendChild(sectionAddPhoto);
+    await divModalAddPhoto.appendChild(iconPicture);
+    await divModalAddPhoto.appendChild(buttonAddPhoto);
+    await divModalAddPhoto.appendChild(textMaxMo);
+    await divModalAddPhoto.appendChild(sectionForm);
+    await divModalAddPhoto.appendChild(form);
+    await divModalAddPhoto.appendChild(formLabelTitre);
+    await divModalAddPhoto.appendChild(formInputTitre);
+    await divModalAddPhoto.appendChild(formLabelCategorie);
+    await divModalAddPhoto.appendChild(formInputCategorie);
+    await divModalAddPhoto.appendChild(divLiseretGris);
+    await divModalAddPhoto.appendChild(formButtonValider);
 }
 
 //function create une balise avec la class donnée en paramétre sinon sans paramétres
@@ -107,6 +181,7 @@ function main() {
 
         editMode();
         createModal();
+        addPhotoGallery();
         console.log("loged");
         console.log("le token index = %s", loged);
     }
