fbc5f5fa89931b67316d6fce78a61458e228868a
GumSurf <gabriel.christe@gmail.com>
2023-04-19 21:57:54 +0200

    Add : icone move photo et fonctionnalité delete de l'icone poubelle

--- Backend/js/index.js
+++ Backend/js/index.js
@@ -47,8 +47,8 @@ async function createModal() {
     //initialisation de toutes les balises destiné a la modal
     const modal = document.querySelector(".modal");
     const divModal = await createBalise("div", "div_modal");
-    const divPhoto =  await createBalise("div", "div_photo");
-    const divButtonTopModal = await createBalise("button", "button_top_modal");
+    const divPhoto = await createBalise("div", "div_photo");
+    const buttonTopModal = await createBalise("button", "button_top_modal");
     const titreModal = await createBalise("h3", "titre_modal", "Gallerie photo");
     //const moveIcon = await createBalise("i");
     //moveIcon.classList.add("fa-solid", "fa-up-down-left-right");
@@ -60,16 +60,22 @@ async function createModal() {
     const deleteAllGallery = await createBalise("a", "delete_all_galery", "Supprimer la galerie");
     const contenuModal = await createBalise("div", "contenu_modal");
     const divFooterModal = await createBalise("div", "div_footer_modal");
+    const divButtonTopModal = await createBalise("div", "div_button_top_modal");
+    const divTitleModal = await createBalise("div", "div_title_modal");
+
 
     modal.appendChild(contenuModal);
     contenuModal.appendChild(divModal);
     contenuModal.appendChild(divPhoto);
-    divModal.appendChild(titreModal);
+    divModal.appendChild(divButtonTopModal);
+    divButtonTopModal.appendChild(buttonTopModal);
+    buttonTopModal.appendChild(iconButtonTop);
+    divModal.appendChild(divTitleModal);
+    divTitleModal.appendChild(titreModal);
     //divModal.appendChild(moveIcon);
     //divModal.before(buttonAddPhoto);
     //divModal.before(deleteAllGallery);
-    divButtonTopModal.appendChild(iconButtonTop);
-    divModal.appendChild(divButtonTopModal);
+
     insertAfter(divFooterModal, contenuModal.lastElementChild);
     divFooterModal.appendChild(buttonAddPhoto);
     divFooterModal.appendChild(deleteAllGallery);
@@ -79,6 +85,7 @@ async function createModal() {
     printWorksModal();
 }
 
+//function pour afficher la galerie
 async function printWorksModal() {
     myFetch = await fetch("http://localhost:5678/api/works");
     myFetch = await myFetch.json();
@@ -89,18 +96,43 @@ async function printWorksModal() {
 
         const textEditer = await createBalise("p", "text_editer", "éditer");
         const trashCanIcone = await createBalise("i");
-        trashCanIcone.classList.add("fa-solid", "fa-trash-can", "fa-2xs");
+        trashCanIcone.classList.add("fa-solid", "fa-trash-can");
+        const buttonTrash = await createBalise("button", "button_trash");
         const worksModalImage = await createBalise("img");
         worksModalImage.src = myFetch[i].imageUrl;
         const modalPhoto = await createBalise("div", "modal_photo");
         const divTrashCan = await createBalise("div", "div_trash_can");
+        const divIconeMove = createBalise("div", "div_icone_move");
+        const iconeMove = createBalise("i", "icone_move");
+        iconeMove.classList.add("fa-solid", "fa-up-down-left-right");
+        
+        buttonTrash.setAttribute("id", myFetch[i].id);
 
-        await divPhoto.appendChild(modalPhoto);
-        await modalPhoto.appendChild(divTrashCan);
-        await divTrashCan.appendChild(trashCanIcone);
         await modalPhoto.appendChild(worksModalImage);
         await modalPhoto.appendChild(textEditer);
+        await divPhoto.appendChild(modalPhoto);
+        await modalPhoto.appendChild(divTrashCan);
+        await divTrashCan.appendChild(buttonTrash);
+        await buttonTrash.appendChild(trashCanIcone);
+        await divTrashCan.before(divIconeMove);
+        await divIconeMove.appendChild(iconeMove);
+
+        divIconeMove.style.visibility = "hidden";
+
+        modalPhoto.addEventListener("mouseenter", () => {
+            divIconeMove.style.visibility = "visible";
+        });
+
+        modalPhoto.addEventListener("mouseleave", () => {
+            divIconeMove.style.visibility = "hidden";
+        });
+
+        buttonTrash.onclick = function () {
+            buttonDeleteOneWork(buttonTrash, modalPhoto);
+        }
     }
+
+
 }
 
 //function création de la modal pour ajouter des photos
@@ -169,24 +201,28 @@ async function addPhotoGallery() {
     printCategories(formSelectCategorie);
 }
 
+//function pour afficher toutes les differents categorie disponible dans le form de la modal add photo
 async function printCategories(formSelectCategorie) {
 
     myFetch = await fetch("http://localhost:5678/api/categories");
     myFetch = await myFetch.json();
 
     for (let index = 0; index < myFetch.length; index++) {
-        if(index === 0) {
+        if (index === 0) {
             const optionSelectDisabled = await createBalise("option", "option_defaut", "");
             optionSelectDisabled.setAttribute("value", "disabled");
-            
+            optionSelectDisabled.setAttribute("id", "0");
+
             await formSelectCategorie.appendChild(optionSelectDisabled);
         }
         const optionSelect = await createBalise("option", "option_select", myFetch[index].name);
+        optionSelect.setAttribute("id", myFetch[index].id);
 
         await formSelectCategorie.appendChild(optionSelect);
     }
 }
 
+//function qui permet d'inserer un element a la fin d'un element
 function insertAfter(newNode, existingNode) {
     existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
 }
@@ -194,31 +230,31 @@ function insertAfter(newNode, existingNode) {
 //function create une balise avec la class donnée en paramétre sinon sans paramétres
 function createBalise(balise, classs, text) {
     const newBalise = document.createElement(balise);
-    if(classs === undefined && text === undefined) {
+    if (classs === undefined && text === undefined) {
         return newBalise;
-    } else if(classs !== undefined && text !== undefined) {
+    } else if (classs !== undefined && text !== undefined) {
         newBalise.classList.add(classs);
         newBalise.textContent = text;
         return newBalise;
-    } else if(classs !== undefined && text === undefined) {
+    } else if (classs !== undefined && text === undefined) {
         newBalise.classList.add(classs);
         return newBalise;
-    } else if(text !== undefined && classs === undefined) {
+    } else if (text !== undefined && classs === undefined) {
         newBalise.textContent = text;
         return newBalise;
-    }else{
+    } else {
         return newBalise;
     }
 }
 
 //function create penToSquareBlack et le texte modifier dans une div définit
 async function addModifierText(div) {
-    if(div === undefined) {
+    if (div === undefined) {
         return;
     } else {
         //create penToSquareBlack
         penToSquare = await createBalise("i"),
-        penToSquare.classList.add("fa-regular", "fa-pen-to-square", "pen_to_square_black")
+            penToSquare.classList.add("fa-regular", "fa-pen-to-square", "pen_to_square_black")
         div.appendChild(penToSquare);
 
         //create text modifier
@@ -226,14 +262,18 @@ async function addModifierText(div) {
         div.appendChild(modifierTxt);
     }
 }
+
+//function pour afficher la modal add photo
 async function openModal() {
     const modalGalery = document.getElementsByClassName("modal")[0];
     const buttonOpenModal = document.getElementsByClassName("button_create_modal")[0];
 
-    buttonOpenModal.onclick = function() {
+    buttonOpenModal.onclick = function () {
         modalGalery.style.display = "flex";
     }
 }
+
+//function pour vérifier si on essaye de fermer une des modals
 async function closeModal() {
     const modalGalery = document.querySelector(".modal");
     const modalAddPhoto = document.querySelector(".modal_add_photo");
@@ -241,29 +281,34 @@ async function closeModal() {
     const buttonCloseModal = document.getElementsByClassName("button_top")[0];
     const arrowLeft = document.querySelector(".button_arrow_left");
 
-    window.onclick = function(event) {
-        if(event.target == modalGalery || event.target == modalAddPhoto) {
+    window.onclick = function (event) {
+        if (event.target == modalGalery || event.target == modalAddPhoto) {
             modalGalery.style.display = "none";
             modalAddPhoto.style.display = "none";
+            resetModalAddPhoto();
         }
     }
 
-    buttonCloseGalery.onclick = function() {
+    buttonCloseGalery.onclick = function () {
         modalGalery.style.display = "none";
         modalAddPhoto.style.display = "none";
+        resetModalAddPhoto();
     }
 
-    buttonCloseModal.onclick = function() {
+    buttonCloseModal.onclick = function () {
         modalAddPhoto.style.display = "none";
         modalGalery.style.display = "none";
+        resetModalAddPhoto();
     }
 
-    arrowLeft.onclick = function() {
+    arrowLeft.onclick = function () {
         modalAddPhoto.style.display = "none";
         modalGalery.style.display = "flex";
+        resetModalAddPhoto();
     }
 }
 
+//function pour afficher l'image avant de la publier dans la galerie
 function readImage() {
     const input = document.querySelector("input");
     const output = document.querySelector("output");
@@ -289,8 +334,8 @@ function readImage() {
     })
 }
 
+//function qui permet de vérifier si il a quelque chose de modifier dans le form
 function postImage() {
-
     const input = document.querySelector("input");
     const select = document.querySelector("select");
     const inputTitre = document.querySelector(".form_input_titre");
@@ -306,34 +351,127 @@ function postImage() {
     });
 }
 
+//function pour modifier la couleur du bouton valider de la modal add photo si ton les champs son remplis
 function callTest() {
     const image = document.querySelector(".image");
     const title = document.querySelector(".form_input_titre");
     const categorie = document.querySelector(".form_select_categorie");
     const button = document.querySelector(".form_button_valider");
 
-    if(image != null && title.value != "" && categorie.value != "disabled") {
+    if (image != null && title.value != "" && categorie.value != "disabled") {
         button.style.background = "#1D6154";
     } else {
         button.style.background = "#A7A7A7";
     }
 }
 
+//function utilisé pour tout les boutons ou autres élements qui seront activés
 function activate() {
     const modalAddPhoto = document.querySelector(".modal_add_photo");
     const buttonModalAddPhoto = document.getElementsByClassName("button_add_photo")[0];
     const modalGalery = document.querySelector(".modal");
+    const formButtonValider = document.querySelector(".form_button_valider");
 
-    buttonModalAddPhoto.onclick = function() {
+    buttonModalAddPhoto.onclick = function () {
         modalAddPhoto.style.display = "flex";
         modalGalery.style.display = "none";
+    }
+
+    formButtonValider.onclick = function () {
+        const image = document.querySelector(".input_image_file");
+        const title = document.querySelector(".form_input_titre");
+        var categorie = document.querySelector(".form_select_categorie");
+
+        idNumber = categorie.selectedIndex;
+
+        const categorieId = categorie.options[idNumber].id;
+
+        const sendWork = new FormData();
+        sendWork.append("image", image.files[0]);
+        sendWork.append("title", title.value);
+        sendWork.append("category", categorieId);
+
+        //envoie d'un Work (photo pour la galerie)
+        const myFetch = fetch('http://localhost:5678/api/works', {
+            method: "POST",
+            headers:
+            {
+                Authorization: `Bearer ${window.localStorage.getItem("tokenUser")}`,
+            },
+            body: sendWork,
+        })
+
+        myFetch.then(function (response) {
+            //Reset la modal add photo
+            if (response.ok) {
+                console.log("Send");
+                resetModalAddPhoto();
+                printWorksModal();
+            } else {
+                console.log("Not Send");
+            }
+        });
+        console.log("button_valider");
+    }
+
+    console.log("modalGalery.style.display == %s", modalGalery.style.display);
+    if (modalGalery.style.display == "none") {
+        console.log("display != none");
+        const modalPhoto = document.querySelector("modal_photo");
+
+        modalPhoto.addEventListener("mouseover", function (event) {
+            console.log("hover");
+        });
+    }
+}
+
+function buttonDeleteOneWork(buttonDelete, modalPhoto) {
+    modalPhoto.remove();
+
+    const id = buttonDelete.getAttribute("id");
+
+    console.log("number = %d", id);
+    fetch(`http://localhost:5678/api/works/${id}`, {
+        method: "DELETE",
+        headers: {
+            Authorization: `Bearer ${window.localStorage.getItem("tokenUser")}`,
+        },
+    }).then((response) => {
+        if (response.ok) {
+            console.log("c'est remove");
+            modalPhoto.remove();
+        }
+    });
+}
+
+function resetModalAddPhoto() {
+    const modalAddPhoto = document.querySelector(".modal_add_photo");
+    const image = document.querySelector(".input_image_file");
+    const title = document.querySelector(".form_input_titre");
+    var categorie = document.querySelector(".form_select_categorie");
+    const iconeImage = document.getElementsByClassName("fa-image")[0];
+    const buttonAddPhotoModal = document.getElementsByClassName("button_add_photo_modal")[0];
+    const textMaxMo = document.getElementsByClassName("text_max_mo")[0];
+    const divAddPhoto = document.getElementsByClassName("div_add_photo")[0]
+
+    modalAddPhoto.style.display = "none";
+    title.value = "";
+    categorie.value = "";
+    image.value = "";
+    iconeImage.style.display = "flex";
+    buttonAddPhotoModal.style.display = "flex";
+    textMaxMo.style.display = "flex";
+    divAddPhoto.style.padding = "30px";
+    const imagePrint = document.querySelector(".image");
+    if (imagePrint) {
+        document.querySelector(".div_image").remove();
     }
 }
 
 async function main() {
     //récupération du token
     const loged = window.localStorage.getItem("tokenUser");
-    if(loged) {
+    if (loged) {
         await editMode();
         await createModal();
         await addPhotoGallery();
