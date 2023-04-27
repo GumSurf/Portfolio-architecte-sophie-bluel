66f9373ed0ab88aa59c55a28989de43e6eba3d0e
GumSurf <gabriel.christe@gmail.com>
2023-04-27 16:38:47 +0200

    Add: fonctionnalité logout

--- Backend/js/login.js
+++ Backend/js/login.js
@@ -9,21 +9,20 @@ form.addEventListener("submit", (event) => {
     const password = inputs[1].value;
 
     const emailPassword = {
-        email : email,
-        password : password
+        email: email,
+        password: password
     };
     console.log("test : %s %s", email, password);
 
     // Appel de la fonction fetch avec toutes les informations nécessaires
-    //**Commentaire à moi même ne pas mettre de s a "api"**
     const myFetch = fetch("http://localhost:5678/api/users/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(emailPassword)
     })
 
-    myFetch.then(function(response) {
-        if(response.ok) {
+    myFetch.then(function (response) {
+        if (response.ok) {
             console.log("Connecté");
             //Récupere l'identifiant id_login
             const idLogin = document.getElementById("id_login");
@@ -31,21 +30,32 @@ form.addEventListener("submit", (event) => {
             idLogin.innerHTML = "Logout";
             stockageToken();
             //redirection vers index.html
-            document.location.href="file:///home/gumsurf/Travail/Projects%20Web/OpenClassRooms/Tuto.HTML5.CSS3/Formation%20Openclassroom/Portfolio-architecte-sophie-bluel/FrontEnd/index.html";
+            document.location.href = "file:///home/gumsurf/Travail/Projects%20Web/OpenClassRooms/Tuto.HTML5.CSS3/Formation%20Openclassroom/Portfolio-architecte-sophie-bluel/FrontEnd/index.html";
         } else {
-            console.log("Pas Connecté");
+            //affichage message d'erreur
+            const messageErreur = document.querySelector(".text_message_erreur");
+            if(messageErreur) {
+                const textMessageErreur = document.querySelector(".text_message_erreur");
+                textMessageErreur.textContent = "Erreur dans l'identifiant ou le mot de passe";
+            } else {
+                const textMessageErreur = document.createElement("p");
+                textMessageErreur.classList.add("text_message_erreur");
+                textMessageErreur.textContent = "Erreur dans l'identifiant ou le mot de passe";
+                const divMessageErreur = document.querySelector(".message_erreur");
+                divMessageErreur.appendChild(textMessageErreur);
+            }
         }
     });
 
     function stockageToken() {
         //obtention du token
         myFetch.then((resp) => resp.json())
-        .then(function(tokenOrNot) {
-            if(tokenOrNot.token) {
-                //Stockage du token d'identification dans le local
-                window.localStorage.setItem("tokenUser", tokenOrNot.token);
-                console.log("j'ai le token = %s", tokenOrNot.token);
-            }
-        });
+            .then(function (tokenOrNot) {
+                if (tokenOrNot.token) {
+                    //Stockage du token d'identification dans le local
+                    window.localStorage.setItem("tokenUser", tokenOrNot.token);
+                    console.log("j'ai le token = %s", tokenOrNot.token);
+                }
+            });
     }
 });
